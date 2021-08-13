00886c6d1478b0958572bb60e669c374c3add2f7
Sun <59905230+ArtidSun@users.noreply.github.com>
2021-08-13 09:58:26 +0700

    start backend

---
+++ src/main/java/int222/project/Dora/DoraApplication.java
@@ -0,0 +1,13 @@
+package int222.project.Dora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/int222/project/Dora/DoraApplicationTests.java
@@ -0,0 +1,13 @@
+package int222.project.Dora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
