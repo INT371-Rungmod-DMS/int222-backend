db12a020c858c898a06c889261c3edc555923bb2
Sun <59905230+ArtidSun@users.noreply.github.com>
2021-10-19 16:46:40 +0700

    Add Json ignore password

--- src/main/java/int222/project/Dora/Models/user.java
+++ src/main/java/int222/project/Dora/Models/user.java
@@ -1,11 +1,14 @@
 package int222.project.Dora.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
+@JsonIgnoreProperties
 @Entity
 public class user {
 
@@ -25,6 +28,7 @@ public class user {
   private String lastName;
   @Getter
   @Setter
+  @JsonIgnore
   private String password;
   @Getter
   @Setter
