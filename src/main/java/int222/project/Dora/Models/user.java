57b43d0e041ab08763458de5c426662e7b2f8f6a
Sun <59905230+ArtidSun@users.noreply.github.com>
2021-11-05 22:47:53 +0700

    add token access

--- src/main/java/int222/project/Dora/Models/user.java
+++ src/main/java/int222/project/Dora/Models/user.java
@@ -2,39 +2,37 @@ package int222.project.Dora.Models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.tomcat.jni.User;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Set;
 
 @JsonIgnoreProperties
+@Table(name = "user")
 @Entity
-public class user {
+@Getter
+@Setter
+public class user implements Serializable {
+    public user() {
+    }
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Getter
-  @Setter
-  private long userId;
-  @Getter
-  @Setter
-  private String userName;
-  @Getter
-  @Setter
-  private String name;
-  @Getter
-  @Setter
-  private String lastName;
-  @Getter
-  @Setter
-  @JsonIgnore
-  private String password;
-  @Getter
-  @Setter
-  private String role;
-  @OneToMany(mappedBy = "user")
-  Set<favorite> favorite;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+    private String userName;
+    private String name;
+    private String lastName;
+    @JsonIgnore
+    private String password;
+    private String role;
+    @OneToMany(mappedBy = "user")
+    Set<favorite> favorite;
 }
+
+
