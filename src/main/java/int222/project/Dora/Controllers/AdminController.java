af3987ad2b18913f1a0f88082b3a3dd1880534c1
Sun <59905230+ArtidSun@users.noreply.github.com>
2021-11-22 23:42:40 +0700

    change remove path

--- src/main/java/int222/project/Dora/Controllers/AdminController.java
+++ src/main/java/int222/project/Dora/Controllers/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin
 @RestController
 public class AdminController {
     @Autowired
