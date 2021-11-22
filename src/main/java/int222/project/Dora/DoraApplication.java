d6bc66e9e76e5d64a974d2aafa7dc0006de1b8ad
Sun <59905230+ArtidSun@users.noreply.github.com>
2021-11-22 23:06:04 +0700

    ADD addCorsMappings METHOD

--- src/main/java/int222/project/Dora/DoraApplication.java
+++ src/main/java/int222/project/Dora/DoraApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigurationProperties({StorageProperties.class})
@@ -17,4 +20,15 @@ public class DoraApplication {
 	}
 	@Bean
 	CommandLineRunner init(StorageService storageService) {return (args) -> {};}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("PUT", "DELETE",
+						"GET", "POST","OPTIONS").allowedHeaders("Authorization").exposedHeaders("Authorization");
+			}
+		};
+	}
 }
